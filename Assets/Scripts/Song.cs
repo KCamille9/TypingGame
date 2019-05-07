4ea0a391c8cb62fe27082d47b47d44449a19dd9c
KCamille9 <bgi_camille@hotmail.com>
2019-05-07 16:42:40 -0600

    first commit

---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Song : MonoBehaviour
+{
+    public string word;
+
+    private int typeIndex;
+
+    public bool isOnScreen;
+
+    WordDisplay display;
+
+    public Song(string _word, WordDisplay _display, bool _isOnScreen)
+    {
+        word = _word;
+        typeIndex = 0;
+        isOnScreen = _isOnScreen;
+
+        display = _display;
+        display.SetWord(word);
+    }
+
+    public WordDisplay GetWordDisplay()
+    {
+        return this.display;
+    }
+
+    public string GetWordString()
+    {
+        return this.word;
+    }
+
+    public void SetWordString(string newWord)
+    {
+        word = newWord;
+    }
+
+    public char GetNextLetter()
+    {
+        return word[typeIndex];
+    }
+
+    public void TypeLetter()
+    {
+        typeIndex++;
+        display.RemoveLetter(); //remove letter from screen
+    }
+
+    public bool WordTyped()
+    {
+        bool wordTyped = (typeIndex >= word.Length);
+        if (wordTyped)
+        {
+            //Remove word from screen
+            display.RemoveWord();
+
+        }
+        return wordTyped;
+    }
+
+    /* public bool InsideBoundaries()
+     {
+
+         //Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+         Vector3 tempos = transform.position;
+
+         if (tempos != null &&  tempos.y >= 0.0){
+             Debug.Log("it's working");
+
+             return false;
+         }
+
+         return true;
+     }*/
+
+}
